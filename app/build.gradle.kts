709063667d55332b4f336cbb69cee5a33c75415c
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-04-23 19:55:19 +0000

    try(chore): shrink apk size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
